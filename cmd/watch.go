591dfa5bd512082db56de7df3d0b7cded6a57d87
Konk32 <131777454+Konk32@users.noreply.github.com>
2026-03-27 20:56:33 +0100

    Changed the W to lowercase :)

--- cmd/watch.go
+++ cmd/watch.go
@@ -12,7 +12,7 @@ import (
 var interval int
 
 var watchCmd = &cobra.Command{
-	Use: "Watch [url]",
+	Use: "watch [url]",
 	Short: "Poll a URL repeadetly until stopped",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
